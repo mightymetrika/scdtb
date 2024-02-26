0273afa57a1e2c22ddbdb8830f86e42a46a5246e
Mackson Ncube <macksonncube@gmail.com>
2024-02-26 19:12:15 +0000

    add extra data set from WWC 5.0

---
+++ data-raw/basic_scd.R
@@ -0,0 +1,8 @@
+## code to prepare `basic_scd` dataset goes here
+basic_scd <- data.frame(phase = c(rep("baseline", 5),
+                                  rep("treatment", 5)),
+                                  time = 1:10,
+                                  socbehavs = c(3, 1, 0, 1, 3,
+                                                16, 11, 13, 15, 10))
+
+usethis::use_data(basic_scd, overwrite = TRUE)
